ac95ed836a0605fc8c85f433a7aed16122c6b03c
trungsliver <buiductrung.cva@gmail.com>
2024-09-27 20:51:16 +0700

    Lesson 1

--- Lesson1.py
+++ Lesson1.py
@@ -1,3 +1,31 @@
+# Câu lệnh hiển thị ra màn hình
 print('Hello world')
 
-# Đây là 1 comment - ghi chú
+# Đây là 1 comment - ghi chú - ctrl /
+
+# Declare variables - Khai báo biến
+class1 = 'MK-C4K-PTB23'
+teacher = 'Bùi Đức Trung'
+
+# Input - nhập dữ liệu từ bàn phím
+name = input('Hãy nhập tên của bạn: ')
+age = input('Hãy nhập tuổi của bạn: ')
+
+# Các cách hiển thị ra màn hình
+    # Cách 1: Dùng dấu +
+print('Họ tên: ' + name)
+    # Cách 2: Dùng dấu ,
+print('Tuổi:', age)
+    # Cách 3: Dùng f - truyền dữ liệu vào string
+print('Tôi tên là ' + name + ' đang ' + age + ' tuổi')
+print('Tôi tên là', name, 'hiện đang', age, 'tuổi')
+print(f'Tôi tên là {name} hiện đang {age} tuổi')
+    # Cách 4: Hiển thị trên nhiều dòng
+print('''
+Em là búp măng non
+Đức Minh là Loopy
+Quang Huy là fan anime
+''')
+
+# Các phép toán cơ bản: + - * /
+print('1 + 1 =', 1+1) 
