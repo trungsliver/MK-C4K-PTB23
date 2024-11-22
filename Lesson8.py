0effa0798915265297216c22af62dc5c0b33fac7
trungsliver <buiductrung.cva@gmail.com>
2024-11-22 19:53:06 +0700

    Lesson 8

--- Lesson8.py
+++ Lesson8.py
@@ -10,25 +10,35 @@ arrHS = ['Tú', 'Lâm', 'Hoàng', 'Huy', 'Bách', 'Minh', 'Bảo']
 # print(len(arr))
 # print(len(arrHS))
 
-# Read - Duyệt, hiện phần tử danh sách
-    # Hiện phần tử bằng chỉ số index (bắt đầu từ 0)
-print(arrHS[4])     # Output: 'Bách'
-    # Hiện phần tử cuối cùng (index = -1)
-print(arrHS[6])     # Output: 'Bảo'
-print(arrHS[-1])    # Output: 'Bảo'
+# # Read - Duyệt, hiện phần tử danh sách
+#     # Hiện phần tử bằng chỉ số index (bắt đầu từ 0)
+# print(arrHS[4])     # Output: 'Bách'
+#     # Hiện phần tử cuối cùng (index = -1)
+# print(arrHS[6])     # Output: 'Bảo'
+# print(arrHS[-1])    # Output: 'Bảo'
 
-    # Duyệt và hiện tất cả phần tử danh sách
-        # Cách 1: Dùng cả index và value
-for i in range(len(arrHS)):
-    print(f'[{i}] {arrHS[i]}')
+#     # Duyệt và hiện tất cả phần tử danh sách
+#         # Cách 1: Dùng cả index và value
+# for i in range(len(arrHS)):
+#     print(f'[{i}] {arrHS[i]}')
 
-        # Cách 2: Chỉ dùng value
-for item in arrHS:
-    print(item)
+#         # Cách 2: Chỉ dùng value
+# for item in arrHS:
+#     print(item)
 
-        # Cách 3: Dùng hàm có sẵn
-for index, value in enumerate(arrHS):
-    print(f'[{index}] {value}')
+#         # Cách 3: Dùng hàm có sẵn
+# for index, value in enumerate(arrHS):
+#     print(f'[{index}] {value}')
 
-        # Cách 4: để test
+#         # Cách 4: để test
+# print(arrHS)
+
+# Update - Chỉnh sửa danh sách
+    # Thêm phần tử vào cuối danh sách - append(value)
+arrHS.append('Nhật')
+    # Thêm phần tử vào vị trí bất kì - insert(index, value)
+arrHS.insert(4, 'Imposter')
+    # Chỉnh sửa value của phần tử
+arrHS[6] = 'Loopy'
+
 print(arrHS)
