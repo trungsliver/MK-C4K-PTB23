c6f4b2f18773bd2e57549e52d2d3bd3a86d00a30
trungsliver <buiductrung.cva@gmail.com>
2024-11-22 19:41:07 +0700

    Lesson 8

--- Lesson8.py
+++ Lesson8.py
@@ -11,3 +11,24 @@ arrHS = ['Tú', 'Lâm', 'Hoàng', 'Huy', 'Bách', 'Minh', 'Bảo']
 # print(len(arrHS))
 
 # Read - Duyệt, hiện phần tử danh sách
+    # Hiện phần tử bằng chỉ số index (bắt đầu từ 0)
+print(arrHS[4])     # Output: 'Bách'
+    # Hiện phần tử cuối cùng (index = -1)
+print(arrHS[6])     # Output: 'Bảo'
+print(arrHS[-1])    # Output: 'Bảo'
+
+    # Duyệt và hiện tất cả phần tử danh sách
+        # Cách 1: Dùng cả index và value
+for i in range(len(arrHS)):
+    print(f'[{i}] {arrHS[i]}')
+
+        # Cách 2: Chỉ dùng value
+for item in arrHS:
+    print(item)
+
+        # Cách 3: Dùng hàm có sẵn
+for index, value in enumerate(arrHS):
+    print(f'[{index}] {value}')
+
+        # Cách 4: để test
+print(arrHS)
