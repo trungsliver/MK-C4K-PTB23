2ffdb4a7cf3f190daae76db09f7e8457f3070cb8
trungsliver <buiductrung.cva@gmail.com>
2024-11-22 19:17:33 +0700

    lesson 8

---
+++ Lesson8.py
@@ -0,0 +1,13 @@
+# Danh sách - Array / List
+# 4 thao tác: CRUD: Create - Read - Update - Delete
+
+# Create - Khởi tạo danh sách
+    # Danh sách rỗng - không có phần tử
+arr = []
+    # Danh sách có sẵn phần tử
+arrHS = ['Tú', 'Lâm', 'Hoàng', 'Huy', 'Bách', 'Minh', 'Bảo']
+    # len() - trả về độ dài / số lượng phần tử của danh sách
+# print(len(arr))
+# print(len(arrHS))
+
+# Read - Duyệt, hiện phần tử danh sách
