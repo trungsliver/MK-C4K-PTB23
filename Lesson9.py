cb6e1d2d2932fa967933a26b0f869f2da3570033
trungsliver <buiductrung.cva@gmail.com>
2024-11-30 10:38:07 +0700

    Lesson 9

---
+++ Lesson9.py
@@ -0,0 +1,11 @@
+# Quy tắc đặt tên file: [Lớp]_[Hoten]_CP2.py
+# Ví dụ: PTB23_DucTrung_CP2.py
+
+# Xuân Tú: 1/2
+# Khải Hoàng: 2/2
+# Quang Huy: 1.5/2
+# Bảo Lâm: 2/2
+# Minh Nhật: 2/2
+# Hoàng Bách: 2/2
+# Đức Minh: 1.5/2
+# Gia Bảo: 1/2
