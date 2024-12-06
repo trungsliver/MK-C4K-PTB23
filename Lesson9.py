15e866371389709eddd7e863b801b6bd56a0cfa9
trungsliver <buiductrung.cva@gmail.com>
2024-12-06 13:43:16 +0700

    Lesson 9

--- Lesson9.py
+++ Lesson9.py
@@ -1,11 +1,12 @@
 # Quy tắc đặt tên file: [Lớp]_[Hoten]_CP2.py
 # Ví dụ: PTB23_DucTrung_CP2.py
 
-# Xuân Tú: 1/2
-# Khải Hoàng: 2/2
-# Quang Huy: 1.5/2
-# Bảo Lâm: 2/2
-# Minh Nhật: 2/2
-# Hoàng Bách: 2/2
-# Đức Minh: 1.5/2
-# Gia Bảo: 1/2
+# Xuân Tú: 1/2 10/10        8
+# Khải Hoàng: 2/2 6/10      8.5
+# Quang Huy: 1.5/2 9/10     9
+# Bảo Lâm: 2/2 9/10         9.5
+# Minh Nhật: 2/2 10/10      9.5
+# Hoàng Bách: 2/2 9/10      9.5
+# Đức Minh: 1.5/2 8/10      8.5
+# Gia Bảo: 1/2 9/10         8.5
+
